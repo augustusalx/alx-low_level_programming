0833455d3a8600d9d6cdb6c21350c4ab690f9761
augustusalx <augustusemmanuel91@gmail.com>
2022-10-16 11:58:38 -0700

    2-print_alphabet.cc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - let alpha be printed in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+
