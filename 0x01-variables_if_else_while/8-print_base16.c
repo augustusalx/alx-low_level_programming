87662119be94bba029d0b3cfe1ef45f8dcd06992
augustusalx <augustusemmanuel91@gmail.com>
2022-10-17 04:08:57 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints single digits in base 10
+ * main - prints the numbers in base 16
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
+	char pn;
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
+
+	for (pn = 'a'; pn <= 'f'; pn++)
+		putchar(pn);
 
 	putchar('\n');
 
