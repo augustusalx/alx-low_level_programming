341f3dadd8b8fa7c110e21c793fab53856f447ac
augustusalx <augustusemmanuel91@gmail.com>
2022-10-19 19:31:53 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld;
 
-	if (ld < 0)
-		ld *= -1;
-
+	if (n < 0)
+		n = -n;
+	ld = n % 10;
 	_putchar(ld + '0');
-
-	return (0);
+	return (n % 10);
 }
-
