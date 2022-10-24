8c3130c67997e202946c997b01b48c60e6015b35
augustusalx <augustusemmanuel91@gmail.com>
2022-10-24 09:39:45 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int t = 0;
+
+	while (*str != '\0')
+	{
+		t++;
+		str++;
+	}
+
+	str -= (t / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n')
+}
+
