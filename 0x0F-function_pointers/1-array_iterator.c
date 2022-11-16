89beaeaa493a9f58b8601fa1eef8af459d456682
augustusalx <augustusemmanuel91@gmail.com>
2022-11-16 15:32:06 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@ lude "function_pointers.h"
  * @array: input integer array
  * @size: size of the array
  * @action: pointer to the function
- * Return: 0
+ * Return: no return
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
